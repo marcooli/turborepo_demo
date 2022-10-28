70b8196b0afa00ca01b5680ad53300a136937405
marcooli <marco.oliveira@rnters.com>
2022-10-28 12:32:09 +0100

    fix: config

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    transpilePackages: ["ui"],
-  },
-};
+const withTM = require("next-transpile-modules")(["ui"]);
+
+
+module.exports = withTM({});
